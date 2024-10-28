a81fe7e0b1c4e1bc19b28ee5a1b7f86d58931c37
Carson <cpsievert1@gmail.com>
2024-10-28 18:32:25 -0500

    Add chatlas examples

---
+++ 01-basics-chatlas.py
@@ -0,0 +1,25 @@
+# -----------------------------------------------------------
+# chatlas is a new package we're working on.
+# It's not yet on PyPI, but you can install it from GitHub:
+# 
+# pip install git+https://github.com/cpsievert/chatlas
+#
+# The README is currently the best place to get an overview
+# https://github.com/cpsievert/chatlas#readme
+# 
+# Also, note that chatlas is essentially the Python equivalent of
+# https://github.com/hadley/elmer/
+# -----------------------------------------------------------
+
+from dotenv import load_dotenv
+from chatlas import ChatOpenAI
+
+load_dotenv()  # Loads OPENAI_API_KEY from the .env file
+
+chat = ChatOpenAI(
+  system_prompt="You are a terse assistant."
+)
+
+chat.chat("What is the capital of the moon?")
+
+chat.chat("Are you sure?")
---
+++ 02-tools-chatlas.py
@@ -0,0 +1,35 @@
+import requests
+from dotenv import load_dotenv
+from chatlas import ChatOpenAI
+
+load_dotenv()  # Loads OPENAI_API_KEY from the .env file
+
+chat = ChatOpenAI(
+  system_prompt=(
+    "You are a helpful assistant that can check the weather. " 
+    "Report results in imperial units."
+  )
+)
+
+# Define a simple tool for getting the current weather
+def get_weather(latitude: float, longitude: float):
+    """
+    Get the current weather for a location using latitude and longitude.
+    """
+    base_url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "current": "temperature_2m,wind_speed_10m,relative_humidity_2m",
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()  # Raise an exception for bad status codes
+        return response.text
+    except requests.RequestException as e:
+        return f"Error fetching weather data: {str(e)}"
+
+
+chat.register_tool(get_weather)
+chat.chat("What is the weather in Seattle?")
